3ecc849b1139b917adcd14a3f94147267b533904
NguyenNgocDung <s3978535@rmit.edu.vn>
2024-08-09 01:33:47 +0700

    final

---
+++ check_import.py
@@ -0,0 +1,6 @@
+try:
+    import face_recognition
+    import face_recognition_models
+    print("Both face_recognition and face_recognition_models imported successfully.")
+except Exception as e:
+    print(f"Error importing modules: {e}")
---
+++ test_imports.py
@@ -0,0 +1,5 @@
+# test_imports.py
+import face_recognition
+import face_recognition_models
+
+print("Imports successful")
