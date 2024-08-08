5f412edf0a9a250a5b017c4e0d6e445f2c38528b
NguyenNgocDung <s3978535@rmit.edu.vn>
2024-08-08 10:14:41 +0700

    Add Docker and Vercel configuration for deployment

--- preinstall.sh
+++ preinstall.sh
@@ -2,8 +2,12 @@
 set -e
 
 apt-get update
-apt-get install -y cmake build-essential libopenblas-dev liblapack-dev
-pip install cmake
+apt-get install -y build-essential libopenblas-dev liblapack-dev wget
+
+# Download and install CMake
+wget https://github.com/Kitware/CMake/releases/download/v3.25.0/cmake-3.25.0-linux-x86_64.sh
+chmod +x cmake-3.25.0-linux-x86_64.sh
+./cmake-3.25.0-linux-x86_64.sh --skip-license --prefix=/usr/local
 
 # Install dlib manually
 pip install dlib
