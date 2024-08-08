efaa543a54023f55d9d4f4676bd863af64e6c711
NguyenNgocDung <s3978535@rmit.edu.vn>
2024-08-08 09:33:43 +0700

    Update requirements and deployment configurations

--- preinstall.sh
+++ preinstall.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-# Exit immediately if a command exits with a non-zero status
 set -e
 
-# Update package lists
 apt-get update
-
-# Install dependencies for dlib
 apt-get install -y cmake build-essential
-
-echo "CMake and build-essential installed successfully"
