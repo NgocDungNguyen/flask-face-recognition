4fe507fb5ab4a1b4e05e0a88f035c281c4e4f428
NguyenNgocDung <s3978535@rmit.edu.vn>
2024-08-08 09:42:29 +0700

    Update requirements and deployment configurations

--- preinstall.sh
+++ preinstall.sh
@@ -2,7 +2,8 @@
 set -e
 
 apt-get update
-apt-get install -y cmake build-essential
+apt-get install -y cmake build-essential libopenblas-dev liblapack-dev
+pip install cmake
 
 # Install dlib manually
 pip install dlib
